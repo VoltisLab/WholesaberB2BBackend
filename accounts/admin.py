c65acb394821f89df50c51633c7af807bdd38ec4
miracle <josephmiracle119@gmail.com>
2025-09-10 18:51:41 +0100

    Expose admin section

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from accounts.models import User
 
-# Register your models here.
+
+admin.site.site_header = "Wholesalers Market Admin Panel"
+admin.site.site_title = "Wholesalers Market Admin"
+admin.site.index_title = "Welcome to Wholesalers Market Dashboard"
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "is_active",
+        "account_type",
+        "first_name",
+        "last_name",
+        "is_superuser",
+    )
+    search_fields = (
+        "email",
+        "account_type",
+    )
